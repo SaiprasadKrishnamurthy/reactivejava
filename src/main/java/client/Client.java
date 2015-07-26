39137013591a7b364c229603b825050fe6dc6a62
saiprasad.krishnamurthy <saiprasad.krishnamurthy@gmail.com>
2015-07-26 11:29:52 +0100

    maven exec plugin added to run the client from command line

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -14,11 +14,13 @@ public class Client {
         Scanner scan = new Scanner(System.in);
         System.out.println(" ########################################### Person search (Non Responsive version) #################################################\n\n");
         System.out.print("Enter the country names (part of) separated by commas (For example: ndia, ger, america)\t:\t");
-        String searchTerm = scan.nextLine();
+        String searchTerm = scan.next();
+        System.out.println();
         System.out.print("Enter how many records you want to retrieve max\t:\t");
         int maxLimit = scan.nextInt();
+        System.out.println();
         System.out.print("Do you want the server to stream the results? (Y|N)\t:\t");
-        String stream = scan.nextLine();
+        String stream = scan.next();
         System.out.println();
 
         if (stream.equalsIgnoreCase("n")) {
